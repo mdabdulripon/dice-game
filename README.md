0bc51a8478420cdb52eff0592ac0752fa51fd2da
mdabdulripon <mdabdulripon@gmail.com>
2019-01-01 18:51:28 -0500

    fix add a flag to know about game status.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## DICE GAME RULES:
+# It is mainly DOM Manipulation 
 
+## DICE GAME RULES:
 - [x] The game has 2 players, playing in rounds
 - [x] In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - [x] BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,49 @@
 // ? Variables
-let scores, currentScore, activePlayer, dice;
+let scores, currentScore, activePlayer, dice, isGameProgress;
 
 appInit();
 
 
 // ? roll the dice
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    // ? create a random function that return 1 to 6 
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
-    
-    // ? display images based on random number 
-    dice.src = '../images/icons/dice-' + randomNumber +'.png';
-    dice.style.display = 'block';
-    
-    // ? Apply Rules 
-    if(randomNumber !== 1) {
-        // ? keep adding the score
-        currentScore += randomNumber;
-        document.querySelector('#current-' + activePlayer).textContent = currentScore;
-    } else {
-        nextPlayer();
+
+    if(isGameProgress) {
+        // ? create a random function that return 1 to 6 
+        let randomNumber = Math.floor(Math.random() * 6) + 1;
+        
+        // ? display images based on random number 
+        dice.src = '../images/icons/dice-' + randomNumber +'.png';
+        dice.style.display = 'block';
+        
+        // ? Apply Rules 
+        if(randomNumber !== 1) {
+            // ? keep adding the score
+            currentScore += randomNumber;
+            document.querySelector('#current-' + activePlayer).textContent = currentScore;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
 
 // ? hold the score
 document.querySelector('.btn-hold').addEventListener('click', function () {
-    // ? add current score to the global scores array;
-    scores[activePlayer] += currentScore;
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-    // ? check if the Player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'You are the Winner!';
-        dice.style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
-    } else {
-        nextPlayer();
+    if(isGameProgress) {
+        // ? add current score to the global scores array;
+        scores[activePlayer] += currentScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+        // ? check if the Player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'You are the Winner!';
+            dice.style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            isGameProgress = false;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
@@ -65,6 +70,7 @@ function appInit() {
     scores = [0, 0];
     currentScore = 0;
     activePlayer = 0;
+    isGameProgress = true;
 
     // ? reset all score to zero;
     document.querySelector('#name-0').textContent = 'Player 1';
@@ -74,12 +80,15 @@ function appInit() {
     document.querySelector('#current-1').textContent = '0';
     document.querySelector('#score-1').textContent = '0';
 
+    // ? remove the winner status if there is any
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
 
+    // ? remove the active player if the game placed before
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     
+    // ? set back to the active player
     document.querySelector('.player-0-panel').classList.add('active');
 
     // ? Hide the dice when page is loading
