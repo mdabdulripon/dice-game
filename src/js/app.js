47a55b4255900bc2ff255fe91ae1abc58e3621ee
mdabdulripon <mdabdulripon@gmail.com>
2019-01-01 18:32:07 -0500

    restucture the js

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,7 @@
 // ? Variables
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, dice;
 
-
-// ? reset all score to zero;
-let playerOneCurrentScore = document.querySelector('#current-0').textContent = '0';
-let playerTwoCurrentScore = document.querySelector('#current-1').textContent = '0';
-
-document.querySelector('#score-0').textContent = '0';
-document.querySelector('#score-1').textContent = '0';
-
-
-// ? Hide the dice when page is loading
-let dice = document.querySelector('.dice');
-dice.style.display = 'none';
-
-
+appInit();
 
 
 // ? roll the dice
@@ -47,8 +32,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
     // ? check if the Player won the game
     if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'You are the Winner!'
-
+        document.querySelector('#name-' + activePlayer).textContent = 'You are the Winner!';
         dice.style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -74,11 +58,31 @@ function nextPlayer () {
     dice.style.display = 'none';
 }
 
-
-
-
-
 // ? New Game
-document.querySelector('.btn-new').addEventListener('click', function () {
-    console.log(`New game is loading`);
-});
+document.querySelector('.btn-new').addEventListener('click', appInit);
+
+function appInit() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+
+    // ? reset all score to zero;
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#current-0').textContent = '0';
+    document.querySelector('#score-0').textContent = '0';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    document.querySelector('#current-1').textContent = '0';
+    document.querySelector('#score-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+
+    // ? Hide the dice when page is loading
+    dice = document.querySelector('.dice');
+    dice.style.display = 'none';
+}
